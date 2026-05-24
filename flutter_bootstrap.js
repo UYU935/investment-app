51663193fbdc9a7a0554a1b35367c4a0949ea3a8
UYU935 <ahisayuki@hotmail.com>
2026-05-24 17:40:41 +0900

    Deploy: language switch, USD conversion, UI fixes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1308460518"
+    serviceWorkerVersion: "3674180117"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,14 +25,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "b47b5134ba58d4db0f66313964f0e3c8",
+"flutter_bootstrap.js": "743bb2121088e7e3694ee2d3a2fa264f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "a49f9ff19b41c19d6669c2d3ba4bdc82",
 "/": "a49f9ff19b41c19d6669c2d3ba4bdc82",
-"main.dart.js": "b8488450ad5d75b96360c7f0cd3e07ec",
+"main.dart.js": "65688b91a8017084595a028107ed0dfd",
 "manifest.json": "2bcee815c0e818572b438b018c612948",
 "version.json": "76959f9e48d32a7133ea6d6eea1aae14"};
 // The application shell files that are downloaded before a service worker can
